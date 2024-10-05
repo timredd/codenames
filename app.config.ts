90b7d96ad7422eeb4150ec383b14e7fd8cfb33e7
timredd <timredd13@gmail.com>
2024-10-05 01:04:00 -0400

    Clean up initial template

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,36 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "@solidjs/start/config";
+import devtools from "solid-devtools/vite";
 
-export default defineConfig({});
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+export default defineConfig({
+  ssr: true,
+  devOverlay: true,
+  server: {
+    preset: "cloudflare-pages",
+    sourceMap: true,
+    minify: false,
+
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    },
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+    build: {
+      sourcemap: true,
+      minify: false,
+      rollupOptions: {
+        external: ["node:async_hooks"],
+      },
+    },
+    plugins: [devtools({ autoname: true })],
+  },
+});
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,10 +1,10 @@
 // @refresh reload
-import { createHandler, StartServer } from "@solidjs/start/server";
+import { StartServer, createHandler } from "@solidjs/start/server";
 
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="en" class="dark">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
